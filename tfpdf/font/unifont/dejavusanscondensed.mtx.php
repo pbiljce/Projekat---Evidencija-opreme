eaa0b6ace35b85f9064e27fe79a2594235b22e39
pbiljce <57287222+pbiljce@users.noreply.github.com>
2020-07-22 10:17:10 +0200

    Add files via upload

---
+++ tfpdf/font/unifont/dejavusanscondensed.mtx.php
@@ -0,0 +1,20 @@
+<?php
+$name='DejaVuSansCondensed';
+$type='TTF';
+$desc=array (
+  'Ascent' => 928.0,
+  'Descent' => -236.0,
+  'CapHeight' => 928.0,
+  'Flags' => 4,
+  'FontBBox' => '[-918 -463 1614 1232]',
+  'ItalicAngle' => 0.0,
+  'StemV' => 87.0,
+  'MissingWidth' => 540.0,
+);
+$up=-63;
+$ut=44;
+// $ttffile='C:\wamp64\www\PHPvjezba\EvidencijaOpreme\tfpdf/font/unifont/DejaVuSansCondensed.ttf';
+$ttffile= __DIR__ . '/DejaVuSansCondensed.ttf';
+$originalsize=680264;
+$fontkey='dejavu';
+?>
